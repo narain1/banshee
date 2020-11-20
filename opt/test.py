1a37b64ef81b18d361cf258b3d1ae0fbf7f0ff62
narain pattabhiraman <narainp@outlook.com>
2020-11-20 20:14:41 +0530

    ヾ(へ)〃

--- opt/test.py
+++
@@ -1,4 +0,0 @@
-import torch
-from torch import nn, optim
-from torch.utils.data import Dataset, DataLoader
-from ..utils.toy_data import make_dataset
--- utils/toy_data.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import make_moons, make_blobs
-
-def make_dataset(seed=13, n_samples=100):
-    np.random.seed(seed)
-    random.seed(seed)
-    x, y = make_moons(n_samples=100, noise=0.1)
-    y = y*2 - 1
-    return x, y
